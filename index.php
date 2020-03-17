4d17e507c6b6a26302dff111f0e1317ea3e621de
João D'Oliveira <AbdulAlhazred200p@users.noreply.github.com>
2020-03-17 11:38:59 -0300

    Doing
    Projeto primário, sem orientação a objetos.

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Conversor</title>
+</head>
+
+<body>
+    <form method="POST" action="conver.php">
+        <label>Digite o texto que deseja converter:</label>
+        <input type="text" name="txtCon" id="txtCon">
+        <br>
+        <br>
+        <label>Selecione a cadeia numérica para qual deseja converter: </label>
+        <input type="submit" value="HEX" name="tipo">
+        <input type="submit" value="DEC" name="tipo">
+        <!-- <input type="submit" value="BIN" name="tipo"> -->
+        
+    </form>
+
+    
+
+</body>
+
+</html>
